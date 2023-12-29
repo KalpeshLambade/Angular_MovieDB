63b498b00b0ff478ec3a37876711a63c5df256f3
kalpesh lambade <kalpeshkishorlambade@gmail.com>
2023-12-30 00:48:22 +0530

    http-service wip

--- movieDb/src/app/app-routing.module.ts
+++ movieDb/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UpcommingComponent } from './pages/upcomming/upcomming.component';
+import { TopRatedComponent } from './pages/top-rated/top-rated.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : HomeComponent
+  },
+  {
+    path : 'upcomming',
+    component : UpcommingComponent
+  },
+  {
+    path : 'top',
+    component : TopRatedComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movieDb/src/app/app.module.ts
+++ movieDb/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UpcommingComponent } from './pages/upcomming/upcomming.component';
+import { TopRatedComponent } from './pages/top-rated/top-rated.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    UpcommingComponent,
+    TopRatedComponent
   ],
   imports: [
     BrowserModule,
