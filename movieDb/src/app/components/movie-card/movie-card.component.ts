a23b7c752897acdc8b9e34352104acb5b67f4d28
kalpesh lambade <kalpeshkishorlambade@gmail.com>
2023-12-30 12:36:11 +0530

    upcomming & top_rated added

--- movieDb/src/app/components/movie-card/movie-card.component.ts
+++ movieDb/src/app/components/movie-card/movie-card.component.ts
@@ -5,10 +5,17 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.css'],
-  encapsulation : ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class MovieCardComponent {
-  @Input() completeMovieList! : MovieResult[]; 
+  @Input() completeMovieList!: MovieResult[];
 
+  constructor() { }
 
+  handleImageError(event: ErrorEvent) {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = 'https://i.ibb.co/6BHp0W8/broken-img.jpg';
+    imgElement.alt = 'broken-img';
+  }
+  
 }
--- movieDb/src/app/pages/upcomming/upcomming.component.ts
+++ movieDb/src/app/pages/upcomming/upcomming.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MovieApiResponse, MovieResult } from 'src/app/interface/response.interface';
+import { MoviedataService } from 'src/app/services/moviedata.service';
 
 @Component({
   selector: 'app-upcomming',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./upcomming.component.css']
 })
 export class UpcommingComponent {
+  movieResult: MovieResult[] = [];
+  isLoading = false;
+  current_page = 1;
+
+  constructor(private movieService: MoviedataService, private router:Router) { }
+
+  ngOnInit() {
+    this.fetchMovies();
+  }
+  
+  fetchMovies() {
+    this.isLoading = true;
+    this.movieService.getMovie(this.current_page,"upcoming").subscribe({
+      next: (responseData: MovieApiResponse) => {
+        this.movieResult.push(...responseData.results);
+        this.isLoading = false;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.isLoading = false;
+        this.router.navigate(['/error']);
+      }
+    });
+    this.current_page++;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !this.isLoading) {
+      this.fetchMovies();
+    }
+  }
 
 }
